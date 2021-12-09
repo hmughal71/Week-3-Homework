d527b3daaafe94019082234239e6bf6099843d54
hmughal71 <johnsamdough@gmail.com>
2021-12-09 20:29:54 +1100

    wrote a pseudocode for html and js

---
+++ Assets/js/script.js
@@ -0,0 +1,46 @@
+
+// when the the button is clicked
+
+// 1. ask  user, how long will the password will be
+// has to be greater than 8 characters and lower than 128 characters
+// 2. to include uppercase?
+// yes
+// 3. to include lowercase?
+// yes
+// 4. to include symbols?
+// yes
+
+
+
+// get all the user inputs
+
+// we need to generate the password
+
+// if the user selected uppercase
+// we need to include all the uppercase character set into our character bank
+
+// if the user selected lowercase
+// we need to include all the lowercase character set into our character bank
+
+// if the user selected numbers
+// we need to include all the number character set into our character bank
+
+// if the user selected symbols
+// we need to include all the symbols character set into our character bank
+
+// at this point we should have our
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
